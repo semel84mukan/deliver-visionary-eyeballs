b194c0394236434c75f5f6bea0baddbe72b53098
semel84mukan <152295085+semel84mukan@users.noreply.github.com>
2023-12-22 18:45:43 +0300

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,12 @@
+        int[] data = new int[10];
+    public static void main(String[] args) {
+}
+
+public class RandomDataGenerator {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+        }
+            System.out.println("Random Number: " + item);
+    }
